9b5a97be119d20630a53159d4011b4f94526de86
Vijay Anand R <vjanandr85@gmail.com>
2017-03-24 20:33:19 +0530

    Update timer_linux.c

--- linux_timer/timer_linux.c
+++ linux_timer/timer_linux.c
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2014  Cisco Systems, All rights reserved.
- *
- * This program and the accompanying materials are made available under
- * the terms of the Eclipse License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
-
 #include <time.h>
 #include <signal.h>
 #include <stdbool.h>
